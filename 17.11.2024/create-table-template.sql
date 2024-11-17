589e7b3ac7e20b93ed9ec00bbf7fa78f3ae926cb
Kikkoust <156084871+Kikkoust@users.noreply.github.com>
2024-11-17 22:24:01 +0200

    movie table lisätty

--- 17.11.2024/create-table-template.sql
+++ 17.11.2024/create-table-template.sql
@@ -1,4 +1,6 @@
-CREATE TABLE genre(
-    genre_id SERIAL Primary KEY,
-    genre_name VARCHAR(255) UNIQUE
+CREATE TABLE movie (
+    movie_id SERIAL PRIMARY KEY,
+    movie_name VARCHAR(255),
+    release_year INT,
+    genre_id INT REFERENCES genre(genre_id)
 );
