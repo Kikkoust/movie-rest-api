c1f3cf4d807cfc694e44347ab105f668bc4a5013
Kikkoust <156084871+Kikkoust@users.noreply.github.com>
2024-11-17 22:35:04 +0200

    review table lisätty

--- 17.11.2024/create-table-template.sql
+++ 17.11.2024/create-table-template.sql
@@ -1,6 +1,7 @@
-CREATE TABLE movie (
-    movie_id SERIAL PRIMARY KEY,
-    movie_name VARCHAR(255),
-    release_year INT,
-    genre_id INT REFERENCES genre(genre_id)
+CREATE TABLE review (
+    review_id SERIAL PRIMARY KEY,
+    review_stars INT CHECK (review_stars >= 0 AND review_stars <=5),
+    review_text TEXT,
+    username VARCHAR(100) REFERENCES registered_user(username),
+    movie_id INT REFERENCES movie(movie_id)
 );
