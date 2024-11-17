20a6b45689b39daa9e84fcbff4f5fe40472e0fad
Kikkoust <156084871+Kikkoust@users.noreply.github.com>
2024-11-17 22:13:12 +0200

    user tablen nimi muutettu registered_user

--- 17.11.2024/create-table-template.sql
+++ 17.11.2024/create-table-template.sql
@@ -1,8 +1,1 @@
-CREATE TABLE "user" (
-    user_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(100),
-    second_name VARCHAR(100),
-    username VARCHAR(100) UNIQUE,
-    password VARCHAR(255),
-    user_birthyear INT
-);
+ALTER Table "user" RENAME TO registered_user;
