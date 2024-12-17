3558fd911a3fa371015c290c923e0d4ccb1f835d
Kikkoust <156084871+Kikkoust@users.noreply.github.com>
2024-12-17 22:43:23 +0200

    server.js post movie lisätty, toiminnallisuus testattu postmanilla, toimii!

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ app.get('/', (req, res) => {
   res.send('<h1>This is the movie API made by MM TIK23SP!</h1><br /><p>/movies</p>');
 });
 
+//MOVIES 
+
 app.get('/movies', async (req, res) => {
   try {
     const result = await client.query('SELECT * FROM movie');
@@ -58,5 +60,21 @@ app.get('/movies/:id', async (req, res) => {
   } catch (err) {
     console.error('Error', err);
     res.status(500).send('Error');
+  }
+});
+
+app.post('/movies', async (req, res) => {
+  const { movie_name, release_year, genre_id } = req.body;
+
+  try {
+    const result = await client.query(
+      'INSERT INTO movie (movie_name, release_year, genre_id) VALUES ($1, $2, $3) RETURNING *',
+      [movie_name, release_year, genre_id]
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error('Error adding movie', err);
+    res.status(500).json({ error: 'Error adding movie' });
   }
 });
