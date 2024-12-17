4af3287f8bf0c4ed62c81f0f62d36d95e79e88c1
Kikkoust <156084871+Kikkoust@users.noreply.github.com>
2024-12-17 22:29:38 +0200

    server.js lisätty GET movies by id, nyt voi hakea elokuvia ID perusteella.

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 const {Client} = pg;
 
 app.listen(3001, () => {
@@ -35,6 +37,24 @@ app.get('/movies', async (req, res) => {
   try {
     const result = await client.query('SELECT * FROM movie');
     res.json(result.rows);
+  } catch (err) {
+    console.error('Error', err);
+    res.status(500).send('Error');
+  }
+});
+
+app.get('/movies/:id', async (req, res) => {
+  const movieId = req.params.id;
+  try {
+    const result = await client.query(
+      'SELECT * FROM movie WHERE movie_id = $1', [movieId]
+    );
+    
+    if (result.rows.length === 0) {
+      return res.status(404).send('<h1>Sorry, no movie here! :(</h1>');
+    }
+
+    res.json(result.rows[0]);
   } catch (err) {
     console.error('Error', err);
     res.status(500).send('Error');
